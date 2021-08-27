2379e2ee804596ec6daa4f006f99afc39a5cbd63
NikohlChip <war2nep@gmail.com>
2021-08-28 02:10:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mixed Messages (js portofolio)
+## What is this?
+- js portofolio
+- private for now
+- Outputs random Fr. Chopin quotes
+
+## Schedule
+Not really deceided.
+
+## Files
+- *portofolio.js* is the target file.
---
+++ portofolio.js
@@ -0,0 +1,6 @@
+const maxim = [
+    "Simplicity is the final achievement. After one has played a vast quantity of notes and more notes, it is simplicity that emerges as the crowning reward of art.", 
+    "Oh, how hard it must be to die anywhere but in one's birthplace.",
+    "If the newspapers cut me up so much that I shall not venture before the world again, I have resolved to become a house painter; that would be as easy as anything else, and I should, at any rate, still be an artist!",
+    "I wish I could throw off the thoughts which poison my happiness, but I take a kind of pleasure in indulging them."
+    ]
